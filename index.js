83151f52e432471f0c1669f450b433f8e324096f
hemanta das <tooglehemanta@gmail.com>
2025-05-04 16:34:59 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').configDotenv();
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.json('Hi, this is AWS ec2 testing');
+})
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('The server is running on ' + process.env.PORT || 3000);
+})
